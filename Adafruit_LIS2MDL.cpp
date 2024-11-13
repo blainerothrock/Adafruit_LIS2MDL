ae0ba972f126c73713a3828d7939152ecfb23400
Adam Garbo <adam.garbo@gmail.com>
2024-11-13 09:37:23 +0100

    Fix Clang Format issues

--- Adafruit_LIS2MDL.cpp
+++ Adafruit_LIS2MDL.cpp
@@ -172,9 +172,9 @@ bool Adafruit_LIS2MDL::_init(void) {
     // No LIS2MDL detected ... return false
     return false;
   }
- 
+
   if (config_a)
-    delete config_a; 
+    delete config_a;
   config_a = new Adafruit_BusIO_Register(i2c_dev, spi_dev, ADDRBIT8_HIGH_TOREAD,
                                          LIS2MDL_CFG_REG_A, 1);
 
